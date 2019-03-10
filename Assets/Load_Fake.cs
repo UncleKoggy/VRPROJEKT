d21af74ec20b4139f6500726a998f5c359c6d54a
UncleKoggy <koggy155@gmail.com>
2019-03-10 18:42:27 +0100

    rdy mechanics

--- Assets/Load_Fake.cs
+++ Assets/Load_Fake.cs
@@ -3,14 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Load_Fake : MonoBehaviour {
-    public Transform kolowrotek, core_fake;
+    public Transform kolowrotek, loadCube;
+    public Rigidbody core;
+
+    public Quaternion startRot;
+    public Vector3 startPos;
+
+    private bool canShoot = false;
     // Use this for initialization
+    private void Awake()
+    {
+        startRot = kolowrotek.rotation;
+        startPos = loadCube.position;
+    }
     void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        float angle = kolowrotek.rotation.eulerAngles.z;
+        angle = angle / 80;
+
+        //Debug.Log(angle);
+        if(angle >= 3.5f)
+        {
+            canShoot = true;
+        }
+        if (!canShoot)
+        {
+            loadCube.position = new Vector3(loadCube.position.x, loadCube.position.y, -angle);
+        }
+    }
+    public void Shoot()
+    {
+        if (canShoot)
+        {
+            //loadCube.position = startPos;
+            //core.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+            kolowrotek.rotation = startRot;
+            canShoot = false;
+        }
+    }
 }
